70810e5ed07dac8aa881ad849e462f20552834b7
Joshua Primero <jprimero155@gmail.com>
2020-04-10 08:22:08 -0700

    builder: Some Updates and Uploader added

--- build_lolz.sh
+++ build_lolz.sh
@@ -1,43 +1,99 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
+ #!/usr/bin/env bash
 
 # Function to show an informational message
 function msg() {
-    echo -e "\e[1;32m$@\e[0m"
+    echo -e "\e[1;32m$*\e[0m"
 }
+
+# Set Chat ID, to push Notifications
+CHATID="-1001222358827"
+
+# Inlined function to post a message
+token="1208711074:AAGaXHkX_suWsyP7E1Uq-yHrIoSGYMlKRqo"
+export BOT_MSG_URL="https://api.telegram.org/bot$token/sendMessage"
+function tg_post_msg {
+	curl -s -X POST "$BOT_MSG_URL" -d chat_id="$CHATID" \
+	-d "disable_web_page_preview=true" \
+	-d "parse_mode=html" \
+	-d text="$1"
+}
+
+# Build Info
+lolz_date="$(date "+%Y%m%d")" # ISO 8601 format
+lolz_friendly_date="$(date "+%B %-d, %Y")" # "Month day, year" format
+builder_commit="$(git rev-parse HEAD)"
+
+# Send a notificaton to TG
+tg_post_msg "<b>clang_builder: LOLZ Clang Compilation Started</b>%0A<b>Date : </b><code>$lolz_friendly_date</code>%0A<b>CLANG Script Commit : </b><code>$builder_commit</code>%0A"
 
 # Build LLVM
 msg "Building LLVM..."
+tg_post_msg "<code>clang_builder: Building LOLZ LLVM</code>"
 ./build-llvm.py \
 	--clang-vendor "LOLZ" \
 	--targets "ARM;AArch64;X86" \
 	--shallow-clone \
-	--lto full \
-	--pgo \
-	--no-update
+	--incremental \
+	--build-type "Release" \
+	--pgo
 
 # Build binutils
 msg "Building binutils..."
+tg_post_msg "<code>clang_builder: Building Binutils</code>"
 ./build-binutils.py --targets arm aarch64 x86_64
 
 # Remove unused products
 msg "Removing unused products..."
+tg_post_msg "<code>clang_builder: Removing unused products...</code>"
 rm -fr install/include
 rm -f install/lib/*.a install/lib/*.la
 
 # Strip remaining products
 msg "Stripping remaining products..."
+tg_post_msg "<code>clang_builder: Stripping remaining products...</code>"
 for f in $(find install -type f -exec file {} \; | grep 'not stripped' | awk '{print $1}'); do
-	strip ${f: : -1}
+	strip "${f: : -1}"
 done
 
 # Set executable rpaths so setting LD_LIBRARY_PATH isn't necessary
 msg "Setting library load paths for portability..."
+tg_post_msg "<code>clang_builder: Setting library load paths for portability...</code>"
 for bin in $(find install -mindepth 2 -maxdepth 3 -type f -exec file {} \; | grep 'ELF .* interpreter' | awk '{print $1}'); do
 	# Remove last character from file output (':')
 	bin="${bin: : -1}"
-
 	echo "$bin"
-	patchelf --set-rpath '$ORIGIN/../lib' "$bin"
+	patchelf --set-rpath "$ORIGIN/../lib" "$bin"
 done
+
+# Release Info
+pushd llvm-project
+llvm_commit="$(git rev-parse HEAD)"
+short_llvm_commit="$(cut -c-8 <<< "$llvm_commit")"
+popd
+
+llvm_commit_url="https://github.com/llvm/llvm-project/commit/$llvm_commit"
+binutils_ver="$(ls | grep "^binutils-" | sed "s/binutils-//g")"
+clang_version="$(install/bin/clang --version | head -n1 | cut -d' ' -f4)"
+tg_post_msg "<b>clang_builder: LOLZ clang compilation Finished</b>%0A<b>Clang Version : </b><code>$clang_version</code>%0A<b>LLVM Commit : </b><code>$llvm_commit_url</code>%0A<b>Binutils Version : </b><code>$binutils_ver</code>"
+
+# Push to GitHub
+# Update Git repository
+tg_post_msg "<code>clang_builder: Preparing for Github Repository..</code>"
+git config --global user.name "Jprimero15"
+git config --global user.email "jprimero155@gmail.com"
+git clone https://github.com/Jprimero15/lolz_clang.git -b master lolz_repo
+pushd lolz_repo
+rm -fr *
+cp -r ../install/* .
+# git checkout README.md # keep this as it's not part of the clang prebuilt itself
+git add .
+git commit -m "Update to $lolz_date Build
+
+LLVM commit: $llvm_commit_url
+binutils version: $binutils_ver
+Builder commit: https://github.com/Jprimero15/lolz-clang-build/commit/$builder_commit"
+
+git push -f
+popd
+
+tg_post_msg "<b>clang_builder: oolchain Compilation Finished and pushed</b>" 
