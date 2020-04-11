87a726ce16ed90041577882052245cbfdb135d71
Joshua Primero <jprimero155@gmail.com>
2020-04-11 18:02:19 +0800

    builder: improve TG messages

--- build_lolz.sh
+++ build_lolz.sh
@@ -22,10 +22,10 @@ lolz_friendly_date="$(date "+%B %-d, %Y")" # "Month day, year" format
 builder_commit="$(git rev-parse HEAD)"
 
 # Send a notificaton to TG
-tg_post_msg "<b>LOLZ Clang Compilation Started</b>%0A<b>Date : </b><code>$lolz_friendly_date</code>%0A<b>CLANG Script Commit : </b><code>$builder_commit</code>%0A"
+tg_post_msg "<b>LOLZ Clang Compilation Started</b>%0A<b>Date: </b><code>$lolz_friendly_date</code>%0A<b>CLANG Script Commit: </b><code>$builder_commit</code>%0A"
 
 # Build LLVM
-tg_post_msg "<code>Building LOLZ LLVM</code>"
+tg_post_msg "<code>Building LLVM...</code>"
 ./build-llvm.py \
 	--clang-vendor "LOLZ" \
 	--targets "ARM;AArch64;X86" \
@@ -35,22 +35,22 @@ tg_post_msg "<code>Building LOLZ LLVM</code>"
 	--pgo
 
 # Build binutils
-tg_post_msg "<code>Building Binutils</code>"
+tg_post_msg "<code>Building Binutils...</code>"
 ./build-binutils.py --targets arm aarch64 x86_64
 
 # Remove unused products
-tg_post_msg "<code>Removing unused products...</code>"
+tg_post_msg "<code>Removing Unused Products...</code>"
 rm -fr install/include
 rm -f install/lib/*.a install/lib/*.la
 
 # Strip remaining products
-tg_post_msg "<code>Stripping remaining products...</code>"
+tg_post_msg "<code>Stripping Remaining Products...</code>"
 for f in $(find install -type f -exec file {} \; | grep 'not stripped' | awk '{print $1}'); do
 	strip "${f: : -1}"
 done
 
 # Set executable rpaths so setting LD_LIBRARY_PATH isn't necessary
-tg_post_msg "<code>Setting library load paths for portability...</code>"
+tg_post_msg "<code>Setting Library Load Paths for Portability...</code>"
 for bin in $(find install -mindepth 2 -maxdepth 3 -type f -exec file {} \; | grep 'ELF .* interpreter' | awk '{print $1}'); do
 	# Remove last character from file output (':')
 	bin="${bin: : -1}"
@@ -66,11 +66,9 @@ popd
 llvm_commit_url="https://github.com/llvm/llvm-project/commit/$llvm_commit"
 binutils_ver="$(ls | grep "^binutils-" | sed "s/binutils-//g")"
 clang_version="$(install/bin/clang --version | head -n1 | cut -d' ' -f4)"
-tg_post_msg "<b>LOLZ Clang Compilation Finished</b>%0A<b>Clang Version : </b><code>$clang_version</code>%0A<b>LLVM Commit : </b><code>$llvm_commit_url</code>%0A<b>Binutils Version : </b><code>$binutils_ver</code>"
 
-# Push to GitHub
-# Update Git repository
-tg_post_msg "<code>Preparing for Github Repository..</code>" 
+# Push to GitHub LOLZ clang builds repo
+tg_post_msg "<code>Pushing New LOLZ Clang Build to Github Repo...</code>" 
 git clone "https://Jprimero15:$GITHUB_TOKEN@github.com/Jprimero15/lolz_clang.git" lolz_repo
 pushd lolz_repo
 rm -fr *
@@ -85,4 +83,4 @@ Builder commit: https://github.com/Jprimero15/lolz-clang-build/commit/$builder_c
 git push
 popd
 
-tg_post_msg "<b>LOLZ Clang Compilation Finished and Pushed</b>" 
+tg_post_msg "<b>LOLZ Clang Compilation Finished</b>%0A<b>Clang Version: </b><code>$clang_version</code>%0A<b>LLVM Commit: </b><code>$llvm_commit_url</code>%0A<b>Binutils Version: </b><code>$binutils_ver</code>"
